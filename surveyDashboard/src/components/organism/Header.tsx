36f817b0b133cb0b60cf82a80bb273bd93a47509
Pravallika <165696096+Pravallika24224@users.noreply.github.com>
2024-07-24 10:24:31 +0530

    Create Header.tsx

---
+++ surveyDashboard/src/components/organism/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import routePaths from '../../assets/routePaths';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from 'surveyDashboard/authContext';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const {auth, updateAuth} = useAuth();
+
+  const handleClick = () => {
+    updateAuth(false, '', 0)
+    navigate(routePaths.login);
+  };
+
+  return (
+    <header className='sticky top-0 flex items-center justify-between flex-wrap bg-blue-950 p-6'>
+      <div className='flex items-center flex-shrink-0 text-white mr-6'>
+        <svg
+          className='fill-current h-8 w-8 mr-2'
+          width='54'
+          height='54'
+          viewBox='0 0 54 54'
+          xmlns='http://www.w3.org/2000/svg'>
+          <path d='M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z' />
+        </svg>
+        {/* <span className='font-semibold text-xl tracking-tight'>Zensar</span> */}
+      </div>
+      {auth.isAuthenticated && 
+      <button
+        onClick={handleClick}
+        className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white'>
+        LogOut
+      </button>
+}
+    </header>
+  );
+};
+
+export default Header;
