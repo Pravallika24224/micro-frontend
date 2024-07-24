19d49449e42f80dce1df1498a6dfb5e8fc575e0d
Pravallika <165696096+Pravallika24224@users.noreply.github.com>
2024-07-24 10:17:59 +0530

    Create routePaths.ts

---
+++ surveyDashboard/src/assets/routePaths.ts
@@ -0,0 +1,11 @@
+const routePaths = {
+  survey_list_dynamic: '/survey-list/:id',
+  survey_list: '/survey-list',
+  survey_details: '/survey-details',
+  dashboard: '/dashboard',
+  create_survey: '/create-survey',
+  login: '/login',
+  admin_details: '/admin-details',
+};
+
+export default routePaths;
